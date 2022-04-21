9c2e10d2d29e24f06f3b0bd012bafa3b02ba46b9
Ichiya Luna <akkyu0724@gmail.com>
2022-04-21 18:56:58 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img class="tachie" src="./assets/pompomrice.png" />
+</template>
+
+<script setup></script>
+
+<style>
+  html,
+  body {
+    width: min-content;
+    margin: 0;
+  }
+  #app {
+    text-align: center;
+  }
+
+  .tachie {
+    max-height: 400px;
+    -webkit-app-region: drag;
+    animation: motion 0.8s linear 0s infinite alternate;
+  }
+
+  @keyframes motion {
+    0% {
+      margin-top: 0px;
+    }
+    100% {
+      margin-top: 10px;
+    }
+  }
+</style>
